27a50ff469bfaf71c3a32e9429158b09b9443478
hiltas73 <h_iltas@yahoo.com>
2020-06-03 14:23:08 +0300

    wed before class

--- src/test/java/com/vytrack/step_definitions/NavigationMenuStepDefs.java
+++ src/test/java/com/vytrack/step_definitions/NavigationMenuStepDefs.java
@@ -5,33 +5,34 @@ import io.cucumber.java.en.When;
 
 public class NavigationMenuStepDefs {
 
-    @When("the user navigates to Fleet--Vehicles")
-    public void the_user_navigates_to_Fleet_Vehicles() {
+    @Then("the user navigate to Fleet, Vehicles")
+    public void the_user_navigate_to_Fleet_Vehicles() {
         System.out.println("the user navigates to Fleet, Vehicles");
     }
 
-    @Then("the title should be Vehicles")
-    public void the_title_should_be_Vehicles() {
-        System.out.println("Expected and Actual title are matching");
+    @Then("the expected title should be Vehicles")
+    public void the_expected_title_should_be_Vehicles() {
+        System.out.println("expected and actual titles are matching");
     }
 
-    @When("the user navigates to Marketing--Campaigns")
-    public void the_user_navigates_to_Marketing_Campaigns() {
-        System.out.println("the user navigates to Marketing--Campaigns");
+    @Then("the user navigate to Marketing, Campaigns")
+    public void the_user_navigate_to_Marketing_Campaigns() {
+        System.out.println("the user navigates to Marketing, Campaigns");
     }
 
-    @Then("the title should be Campaigns")
-    public void the_title_should_be_Campaigns() {
-        System.out.println("Expected and Actual title are matching");
+    @Then("the expected title should be Campaigns")
+    public void the_expected_title_should_be_Campaigns() {
+        System.out.println("expected and actual titles are matching");
     }
 
-    @When("the user navigates to Activities--Calendar Events")
-    public void the_user_navigates_to_Activities_Calendar_Events() {
-        System.out.println("the user navigates to Activities--Calendar Events");
+    @Then("the user navigate to Activities, Calendar Events")
+    public void the_user_navigate_to_Activities_Calendar_Events() {
+        System.out.println("the user navigates to Activities, Calendar Events");
     }
 
-    @Then("the title should be Calendar Events")
-    public void the_title_should_be_Calendar_Events() {
-        System.out.println("Expected and Actual title are matching");
+    @Then("the expected title should be Calendar Events")
+    public void the_expected_title_should_be_Calendar_Events() {
+        System.out.println("expected and actual titles are matching");
     }
+
 }
--- src/test/resources/features/NavigationMenu.feature
+++ src/test/resources/features/NavigationMenu.feature
@@ -1,19 +1,19 @@
-@SalesLogin
-Feature: Sales manager should be able to Navigate Menu
-  Scenario: Navigate to Fleet--Vehicles
+@SalesNavigate
+Feature: Navigate Menu
+  Scenario: Navigate to Fleet, Vehicles
     Given the user is on the login page
-    And the user enter sales manager information
-    When the user navigates to Fleet--Vehicles
-    Then the title should be Vehicles
+    When the user enter sales manager information
+    Then the user navigate to Fleet, Vehicles
+    Then the expected title should be Vehicles
 
-  Scenario: Navigate to Marketing--Campaigns
-    Given the user is on the login page
-    And the user enter sales manager information
-    When the user navigates to Marketing--Campaigns
-    Then the title should be Campaigns
+  Scenario: Navigate to Marketing, Campaigns
+      Given the user is on the login page
+      When the user enter sales manager information
+      Then the user navigate to Marketing, Campaigns
+      Then the expected title should be Campaigns
 
-  Scenario: Navigate to Activities--Calendar Events
-    Given the user is on the login page
-    And the user enter sales manager information
-    When the user navigates to Activities--Calendar Events
-    Then the title should be Calendar Events
+    Scenario: Navigate to Activities, Calendar Events
+      Given the user is on the login page
+      When the user enter sales manager information
+      Then the user navigate to Activities, Calendar Events
+      Then the expected title should be Calendar Events
